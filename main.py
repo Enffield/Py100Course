e2674ed2ccc21f570c596e2e35ffca1c788ef041
Enffield <55981013+Enffield@users.noreply.github.com>
2024-03-27 19:44:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,18 @@
-import time, os
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
 
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
 
-def view_table(data):
-    print(f'|{"Имя":20}|{"Возраст":20}|')
-    print('_' * 43)
-    for person in data:
-        print(f'|{person["name"]:20}|{person["age"]:<20}|')
-    print('_' * 43)
+i = 0
+while True:
+    money_capital += salary - spend
+    spend *= 1 + increase
+    if money_capital > 0:
+        i += 1
+        continue
+    break
 
-
-def clear_table(data: list):
-    data.clear()
-
-
-spisok = [{'name': 'Pavel', 'age': '30'}, {'name': 'Andrey', 'age': '22'},
-          {'name': 'Egor', 'age': '27'}]
-view_table(spisok)
-
-answer = input("Хочешь добавить данных?\n")
-if answer.lower() == 'да':
-    print("Если хочешь завершить ввод данных, то введи 0")
-    while True:
-        data = input("Введи имя возраст разделенные пробелом: ")
-        if data == "0":
-            print("Пользователь ввел все, что хотел")
-            break
-        sep_data = data.split()
-        if len(sep_data) != 2:
-            print("Должно быть 2 значения: Имя Возраст")
-            continue
-        if not sep_data[0].isalpha():
-            print("Имя должно быть из букв")
-            continue
-        if not sep_data[1].isdigit():
-            print("Возраст должен быть из цифр")
-            continue
-        spisok.append({'name': sep_data[0], 'age': int(sep_data[1])})
-
-    view_table(spisok)
-
-os.system('cls')
-
-answer = input("Хочешь очистить данные?\n")
-if answer.lower() == 'да':
-    clear_table(spisok)
-    view_table(spisok)
-
-
+print("Количество месяцев, которое можно протянуть без долгов:", i)
 
