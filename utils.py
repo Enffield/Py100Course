f9cdb85b5f7bb58459a1f15d7873b20dc562dc8c
Enffield <55981013+Enffield@users.noreply.github.com>
2024-04-03 21:24:47 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -16,3 +16,14 @@ if __name__ == '__main__':
     files_to_delete = ["output.txt", "output.json", "output.csv"]
 
     delete_files_recursive(folder_path, files_to_delete)
+
+# Решение функции с частотным анализом 
+def count_letters(str_):
+    a = {}
+    for char in str_:
+        if char.isalpha():
+            if char in a.keys():
+                a[char] += 1
+            else:
+                a[char] = 1
+    return a
